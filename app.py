24986024c3cad34b1063304615503e9b4a1417e1
Kaushie Kodes <65993031+kausalyanp@users.noreply.github.com>
2024-11-16 20:17:24 +0530

    Update app.py
    gfhgh

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-    app.run(host="0.0.0.0", port=8082)
 
 # Simulated database (in-memory list)
 tasks = []
