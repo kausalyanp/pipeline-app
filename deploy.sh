e2f5479aa93abd8da84999b5f53a2ff417759162
Kaushie Kodes <65993031+kausalyanp@users.noreply.github.com>
2024-11-17 20:06:48 +0530

    Update deploy.sh
    4

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ if [ "$ENV" == "development" ]; then
     docker run -d -p 9090:5000 -e FLASK_PORT=5000 my-app:${BUILD_NUMBER}
 elif [ "$ENV" == "staging" ]; then
     echo "Deploying to Staging..."
-    docker run -d -p 8081:5001 -e FLASK_PORT=5001 my-app:${BUILD_NUMBER}
+    docker run -d -p 8081:5000 -e FLASK_PORT=5001 my-app:${BUILD_NUMBER}
 elif [ "$ENV" == "production" ]; then
     echo "Deploying to Production..."
-    docker run -d -p 8082:5002 -e FLASK_PORT=5002 my-app:${BUILD_NUMBER}
+    docker run -d -p 8082:5000 -e FLASK_PORT=5002 my-app:${BUILD_NUMBER}
 else
     echo "Invalid environment specified!"
     exit 1
